efe7b0e2ec22cb56ed9e3c0cd4e4fd96ad551a99
samithspalan <samithpoojary12345@gmail.com>
2025-02-09 23:48:51 +0530

    first commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+class Solution {
+    int arraySum(int arr[]) {
+        int a=0;
+        for(int i=0;i<arr.length;i++){
+            
+            a=a+arr[i];
+            
+        }
+       return a;
+    }
+}
