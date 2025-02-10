2eb29eef49b9c322e7e043d3ff8cc52a5b0091df
samithspalan <samithpoojary12345@gmail.com>
2025-02-10 20:25:26 +0530

    first commit

---
+++ fibonacci.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n;
+		System.out.println("enter a no");
+		n=s.nextInt();
+		int fib=find(n);
+	  
+	 System.out.println("fibnocci of"+n+" is "+fib);
+
+
+		
+	}
+	public static int find(int n){
+		 if(n==0){
+			 return 0;
+		 }
+		 else if (n==1){
+			 return 1;
+
+		 }
+		 else{
+			 return find(n-1)+find(n-2);
+		 }
+	 }
+}
