010f9d27ed334c89e52559a2f396b29001a14bac
samithspalan <samithpoojary12345@gmail.com>
2025-02-11 18:12:41 +0530

    first commit

--- fibo.java
+++ fibo.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-public class Main {
-
-
+public class fibo {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n;
---
+++ whil.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class whil {
+    
+    public static void main(String args[]) {
+        Scanner s=new Scanner(System.in);
+        
+      int n=s.nextInt();
+      int i=10;
+      while(i>0){
+          System.out.print(n*i+" ");
+          i--;
+          
+          
+      }
+      
+    }
+}
+
