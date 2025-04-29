3027d6542aa0b991cf85495d0c6e6bfddd44bab9
Samith S Palan <samithpoojary12345@gmail.com>
2025-04-29 22:39:24 +0530

    Create RotateBy90Degree.java

---
+++ RotateBy90Degree.java
@@ -0,0 +1,14 @@
+
+// class Solution:
+    
+//     #Function to rotate matrix anticlockwise by 90 degrees.
+//     def rotateby90(self, mat): 
+//         n=len(mat)
+//         res=[[0]*n for _ in range(n)]
+//         for i in range(n):
+//             for j in range(n):
+//                 res[n-j-1][i]=mat[i][j]
+//         for i in range (n):
+//             for j in range(n):
+//                 mat[i][j]=res[i][j]
+       
