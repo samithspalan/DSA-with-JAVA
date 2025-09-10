e565e633629bd9ec8166bd6d6bd40e01ef971c67
samithspalan <samithpoojary12345@gmail.com>
2025-09-11 03:44:33 +0530

    Update Count1.java and IDE configuration

--- src/Count1.java
+++ src/Count1.java
@@ -1,6 +1,19 @@
 public class Count1 {
     public static void main(String[] args) {
-        int[] arr={1,0,1,1,1,0,0,1,0};
-
+        int[] arr = {1, 0, 1, 1, 1, 0, 0, 1, 0,1,1,1,1,1,0};
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+              if(max<count){
+                  max=count;
+              }
+            }
+            if(arr[i]==0){
+                count=0;
+            }
+        }
+        System.out.println(max);
     }
 }
